96b5fd7636f2aac5c0bd85f43d9638269461d979
StorlerHua <1187692525@qq.com>
2018-04-22 21:45:34 +0800

    defer panic recover

--- hellogoland/defer.go
+++ hellogoland/defer.go
@@ -1,19 +1,59 @@
 package main
 
 import "fmt"
+//https://blog.go-zh.org/defer-panic-and-recover
+func f() (i int)  {
+	defer func() {i++}()
+	return 1
+}
 
-func main() {
-	i :=1
-	// 异步 --压栈，推迟的函数调用会被压入一个栈中。当外层函数返回时，被推迟的函数会按照后进先出的顺序调用。
-	defer fmt.Println(i)
-	i+=1
-	fmt.Println(i)
+func x() {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered in f", r)
+		}
+	}()
+	fmt.Println("Calling g.")
+	g(0)
+	fmt.Println("Returned normally from g.")
+}
 
-	fmt.Println("counting")
-
-	for i := 0; i < 10; i++ {
-		defer fmt.Println(i)
+func g(i int) {
+	if i > 3 {
+		fmt.Println("Panicking!")
+		panic(fmt.Sprintf("%v", i))
 	}
+	defer fmt.Println("Defer in g", i)
+	fmt.Println("Printing in g", i)
+	g(i + 1)
+}
 
-	fmt.Println("done")
+func c() int  {
+	i :=1
+	defer fmt.Println(i)
+	defer func() {i++
+	fmt.Println(i)
+	}()
+	return i
+}
+func main() {
+	x()
+	fmt.Println("Returned normally from f.")
+
+	// Deferred functions may read and assign to the returning function's named return values .
+	fmt.Println(f())
+	fmt.Println(c())
+	//i :=1
+	//// 异步 --压栈，推迟的函数调用会被压入一个栈中。当外层函数返回时，被推迟的函数会按照后进先出的顺序调用。
+	//defer fmt.Println(i)
+	//i+=1
+	//fmt.Println(i)
+	//
+	//fmt.Println("counting")
+	//
+	//for i := 0; i < 10; i++ {
+	//	defer fmt.Println(i)
+	//}
+	//
+	//fmt.Println("done")
 }
